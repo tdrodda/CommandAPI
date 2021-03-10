71e366a1143887170ca6dc861608062508798257
tdrodda <tdrodda@gmail.com>
2021-03-09 21:03:10 -0500

    SECRETS IMPLIMENTATION FOR CONNECTION-STRING

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 namespace CommandAPI
 {
@@ -34,10 +35,18 @@ namespace CommandAPI
         public void ConfigureServices(IServiceCollection services)
         {
 
+
+            var builder = new NpgsqlConnectionStringBuilder();
+            builder.ConnectionString = Configuration.GetConnectionString("PostgreSqlConnection");
+            builder.Username = Configuration["UserID"];
+            builder.Password = Configuration["Password"];
+
+
             //this registers the CommandContext (in Service Container) and 
             //point it to the connectionString (PostgreSqlConnection) that 
             //resides on the appsettings.json file
-            services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(Configuration.GetConnectionString("PostgreSqlConnection")));
+            //services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(Configuration.GetConnectionString("PostgreSqlConnection")));
+            services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(builder.ConnectionString));
 
             services.AddControllers();
             #region "Removed Code that registers ICommandAPIRepo
